358144d406185793a267ae1f0b58f85be8f93bc5
Andreja011 <andrejasimonovic011@gmail.com>
2022-11-20 14:21:15 +0100

    Added hover effect

--- script.js
+++ script.js
@@ -7,11 +7,18 @@ const header = document.querySelector(".header-one");
 const grid = document.createElement("div");
 grid.classList.add("grid");
 container.appendChild(grid);
+
 // Creating grid elements
 const fragment = document.createDocumentFragment();
 for (let i = 0; i < 256; i++) {
   const gridElement = document.createElement("div");
   gridElement.classList.add("grid-element");
+  // Adding hover effect
+  gridElement.addEventListener("mouseover", function () {
+    gridElement.classList.toggle("grid-element-hover-effect");
+  });
   fragment.appendChild(gridElement);
 }
 grid.appendChild(fragment);
+
+// Adding hover effect
