1fb59e93f62e1fbc135e17c480b0e7f355039437
Andreja011 <andrejasimonovic011@gmail.com>
2022-11-20 17:32:36 +0100

    a

--- script.js
+++ script.js
@@ -8,20 +8,24 @@ const grid = document.createElement("div");
 grid.classList.add("grid");
 container.appendChild(grid);
 
+const randomColor = function () {
+  const color = "#" + (((1 << 24) * Math.random()) | 0).toString(16);
+  return color;
+};
 // Creating grid elements
 const fragment = document.createDocumentFragment();
 for (let i = 0; i < 256; i++) {
   const gridElement = document.createElement("div");
   gridElement.classList.add("grid-element");
 
-  // Adding hover effect
-  gridElement.addEventListener("mouseover", function () {
-    gridElement.classList.toggle("grid-element-hover-effect");
+  // Adding hover effect and random color
+  document.documentElement.style.setProperty(
+    "--background-color",
+    randomColor()
+  );
+  gridElement.addEventListener("mouseover", (event) => {
+    event.target.style.backgroundColor = randomColor();
   });
   fragment.appendChild(gridElement);
 }
 grid.appendChild(fragment);
-
-// Create random color
-const randomColor = "#" + (((1 << 24) * Math.random()) | 0).toString(16);
-document.documentElement.style.setProperty("--background-color", randomColor);
