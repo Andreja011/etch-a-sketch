814bdacaaee508115086b929050f7e99e3714e14
Andreja011 <andrejasimonovic011@gmail.com>
2022-11-22 22:33:54 +0100

    Added button with prompt function

--- script.js
+++ script.js
@@ -30,3 +30,28 @@ for (let i = 0; i < 256; i++) {
   fragment.appendChild(gridElement);
 }
 grid.appendChild(fragment);
+
+// Adding prompt button
+const button = document.createElement("button");
+button.textContent = "Size";
+button.classList.add("button-prompt");
+container.appendChild(button);
+
+// Adding random color on button for fun :P
+button.addEventListener("mouseover", (event) => {
+  event.target.style.backgroundColor = randomColor();
+});
+
+// Adding prompt function
+button.addEventListener("click", () => {
+  const userNumber = function () {
+    const userChoise = Number(prompt("Choose a number from 1 to 100."));
+    if (userChoise <= 100) {
+      console.log(userChoise);
+      return userChoise;
+    } else {
+      userNumber();
+    }
+  };
+  userNumber();
+});
